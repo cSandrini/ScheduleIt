463da8575d250e39b128309c99df0714f2f20383
Pedro Henrique Caron Sandrini <20191TIPI0082@cefetes.br>
2022-06-22 10:29:29 -0300

    Final 22/06

--- scheduleit/model/cadastrosDAO.php
+++ scheduleit/model/cadastrosDAO.php
@@ -2,7 +2,7 @@
 require_once "../funcoesUteis.php";
 
 function cadastrarUsuario($conexao, $nome, $sobrenome, $cpf, $numero, $email, $senha) {
-    $sql = "INSERT INTO ezbook.usuarios (nome, sobrenome, cpf, numero, email, senha) VALUES ('$nome', '$sobrenome', '$cpf', '$numero', '$email', '$senha');";
+    $sql = "INSERT INTO scheduleit.usuarios (nome, sobrenome, cpf, numero, email, senha) VALUES ('$nome', '$sobrenome', '$cpf', '$numero', '$email', '$senha');";
     $resultado = mysqli_query( $conexao, $sql ) or die( mysqli_error($conexao) );
 }
 
