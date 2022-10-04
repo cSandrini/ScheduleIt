10efba458766c13b2419360a34bac04dceafdb59
Pedro Henrique <pedrocsandrini@gmail.com>
2022-10-04 15:19:46 -0300

    image logo

--- scheduleit/view/pages/sala/sala.php
+++ scheduleit/view/pages/sala/sala.php
@@ -12,7 +12,8 @@
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link href="../../styles/css/cover.css" rel="stylesheet">
-  <script src="assets/js.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="js.js"></script>
 
 </head>
 
