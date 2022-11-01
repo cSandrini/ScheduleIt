61c3ff647f191696c3e50a1f6553a0df9b1059b6
Pedro Henrique <pedrocsandrini@gmail.com>
2022-10-31 21:38:17 -0300

    funcionario display perfil bug fix

--- scheduleit/view/pages/sala/sala.php
+++ scheduleit/view/pages/sala/sala.php
@@ -59,15 +59,17 @@
                     $foto = $row->foto;
                     if(!$foto) {
                       $foto = addslashes('../../styles/blank.png');
+                      $imgTag = "<img class='rounded imgsala me-2' style='padding: 0!important; width: 80px; height: 80px;'  src='$foto'>";
                     } else {
-                      $foto = addcslashes('data:imgLogo/jpeg;base64,base64_encode',$foto);
+                      $foto = base64_encode($foto);
+                      $imgTag = "<img class='rounded imgsala me-2' style='padding: 0!important; width: 80px; height: 80px;'  src='data:image/png;base64,$foto'>";
                     }
                     $i++;
                     echo  "<div class='funcionarioDisplay border rounded bg-white me-3 mb-3 p-0'>
                             <div class='m-0 p-2 d-flex align-items-center gallery_product' onclick='redirectAgenda()'>
-                                <div class='d-inline'>
-                                    <img class='rounded imgsala me-2' style='padding: 0!important; width: 80px; height: 80px;'  src='$foto'>
-                                </div>
+                                <div class='d-inline'>"
+                                  ,$imgTag,
+                                "</div>
                                 <div class='d-inline lh-1'>
                                     <span style='max-width: 220px;' class='d-inline-block text-truncate mb-1 fw-bold title-card'>$row->nome</span>
                                     <p class='m-0'><small>Função</small></p>
